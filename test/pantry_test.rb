bebd59974a4cad7aef807bf2d93e913817bcacc9
Balmung421 <zcook421@gmail.com>
2017-08-01 09:10:41 -0600

    Added passing exist test for Pantry class

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,13 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class PantryTest < Minitest::Test
+
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 
 end
